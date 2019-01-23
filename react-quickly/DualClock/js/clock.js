411c0016804cce0ac8585b06255e7304208503e2
Min Xu <xukmin@gmail.com>
2019-01-22 20:05:18 -0800

    Add an example of both digital and analog clocks.

---
+++ react-quickly/DualClock/js/clock.js
@@ -0,0 +1,28 @@
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.launchClock();
+    this.state = {
+      currentTime: new Date().toLocaleString()
+    };
+  }
+
+  launchClock() {
+    setInterval(() => {
+      console.log('Updating time...');
+      this.setState({
+        currentTime: new Date().toLocaleString()
+      });
+    }, 1000);
+  }
+
+  render() {
+    console.log('Rendering...');
+    return React.createElement(
+      'div',
+      null,
+      React.createElement(AnalogDisplay, { time: this.state.currentTime }),
+      React.createElement(DigitalDisplay, { time: this.state.currentTime })
+    );
+  }
+}
---
+++ react-quickly/DualClock/jsx/clock.jsx
@@ -0,0 +1,28 @@
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.launchClock();
+    this.state = {
+      currentTime : (new Date()).toLocaleString()
+    }
+  }
+  
+ launchClock () {
+   setInterval(() => {
+      console.log('Updating time...');
+      this.setState({
+        currentTime : (new Date()).toLocaleString()
+      })
+    }, 1000)
+ }
+
+  render () {
+    console.log('Rendering...');
+    return (
+      <div>
+        <AnalogDisplay time={this.state.currentTime}/>
+        <DigitalDisplay time={this.state.currentTime}/>
+      </div>
+    )
+ }
+}
