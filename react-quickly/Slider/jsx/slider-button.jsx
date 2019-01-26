f60f6ae84bc6af1261944e430e5bbfa117eff693
Min Xu <xukmin@gmail.com>
2019-01-26 15:35:21 -0800

    Add a component for integrating buttons.

---
+++ react-quickly/Slider/jsx/slider-button.jsx
@@ -0,0 +1,46 @@
+class SliderButtons extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sliderValue: 0
+    }
+  }
+
+  handleSlide(event, ui) {
+    this.setState({
+      sliderValue: ui.value
+    })
+  }
+
+  handleChange(value) {
+    return () => {
+      $('#slider').slider('value', this.state.sliderValue + value);
+      this.setState({
+        sliderValue: this.state.sliderValue + value
+      })
+    }
+  }
+
+  componentDidMount() {
+    $('#slider').on('slider', this.handleSlide);
+  }
+
+  componentWillUnmount() {
+    $('#slider').off('slider', this.handleSlide);
+  }
+
+  render() {
+    return <div>
+      <button disabled={(this.state.sliderValue < 1)? true : false}
+        className="btn default-btn"
+        onClick={this.handleChange(-1)}>
+          1 Less ({this.state.sliderValue - 1})
+      </button>
+      <button disabled={(this.state.sliderValue > 99)? true : false}
+        className="btn default-btn"
+        onClick={this.handleChange(1)}>
+          1 More ({this.state.sliderValue + 1})
+      </button>
+    </div>
+  }
+}
