330362cd74dd529af077a60db840e7dc73327ab0
Min Xu <xukmin@gmail.com>
2019-01-30 13:46:38 -0800

    Add Tooltip project from Chaper 10.

---
+++ react-quickly/Tooltip/js/script.js
@@ -0,0 +1,16 @@
+ReactDOM.render(React.createElement(
+  "div",
+  null,
+  React.createElement(
+    Tooltip,
+    { text: "The book you've reading now" },
+    "React Quickly "
+  ),
+  "was published in 2017. It's awesome!",
+  React.createElement(
+    Tooltip,
+    { text: "Really? test the length" },
+    " Test one more case "
+  ),
+  "It's good!"
+), document.getElementById('tooltip'));
---
+++ react-quickly/Tooltip/jsx/script.jsx
@@ -0,0 +1,9 @@
+ReactDOM.render(
+  <div>
+    <Tooltip text="The book you've reading now">React Quickly </Tooltip>
+     was published in 2017. It's awesome!
+    <Tooltip text="Really? test the length"> Test one more case </Tooltip>
+    It's good!
+  </div>,
+  document.getElementById('tooltip')
+)
