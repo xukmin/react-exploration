5806789a827e64d9bf61f6717f2c7b2136745fe4
Min Xu <xukmin@gmail.com>
2019-02-24 20:12:59 -0800

    Update Timer.

--- react-quickly/Timer/jsx/timer-wrapper.jsx
+++ react-quickly/Timer/jsx/timer-wrapper.jsx
@@ -2,9 +2,12 @@ class TimerWrapper extends React.Component {
   constructor(props) {
     super(props);
     this.startTimer = this.startTimer.bind(this);
+    this.stopTimer = this.stopTimer.bind(this);
+    this.resumeTimer = this.resumeTimer.bind(this);
     this.state = {
       timeLeft: null,
-      timer: null 
+      timer: null,
+      pause: false
     }
   }
 
@@ -22,6 +25,17 @@ class TimerWrapper extends React.Component {
     });
   }
 
+  stopTimer() {
+    clearInterval(this.state.timer);
+    this.setState({timer: null});
+  }
+
+  resumeTimer() {
+    if (this.state.timeLeft > 0) {
+      this.startTimer(this.state.timeLeft);
+    }
+  }
+
   render() {
     return <div className="row-fluid">
       <h2>Timer</h2>
@@ -31,6 +45,21 @@ class TimerWrapper extends React.Component {
         <Button timerLabel="15" startTimer={this.startTimer}/>
       </div>
       <Timer timeLeft={this.state.timeLeft}/>
+      {this.state.timeLeft > 0 &&
+      <div>
+        <div className="btn-group" role="group">
+          {this.state.timer === null ?
+          <button className="btn-success btn" onClick={this.resumeTimer}>
+            Resume
+          </button>
+          :
+          <button className="btn-warning btn" onClick={this.stopTimer}>
+            Pause
+          </button>
+          }
+        </div>
+      </div>
+      }
       <audio id="end-of-time" src="flute_c_long_01.wav" preload="auto"></audio>
     </div>
   }
