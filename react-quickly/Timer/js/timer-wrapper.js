95ef81b40467eef08b1170f643dda60a6e7ec531
Min Xu <xukmin@gmail.com>
2019-02-25 20:20:06 -0800

    Update timer-wrapper js file.

--- react-quickly/Timer/js/timer-wrapper.js
+++ react-quickly/Timer/js/timer-wrapper.js
@@ -2,9 +2,12 @@ class TimerWrapper extends React.Component {
   constructor(props) {
     super(props);
     this.startTimer = this.startTimer.bind(this);
+    this.stopTimer = this.stopTimer.bind(this);
+    this.resumeTimer = this.resumeTimer.bind(this);
     this.state = {
       timeLeft: null,
-      timer: null
+      timer: null,
+      pause: false
     };
   }
 
@@ -20,6 +23,17 @@ class TimerWrapper extends React.Component {
       timeLeft: timeLeft,
       timer: timer
     });
+  }
+
+  stopTimer() {
+    clearInterval(this.state.timer);
+    this.setState({ timer: null });
+  }
+
+  resumeTimer() {
+    if (this.state.timeLeft > 0) {
+      this.startTimer(this.state.timeLeft);
+    }
   }
 
   render() {
@@ -39,6 +53,23 @@ class TimerWrapper extends React.Component {
         React.createElement(Button, { timerLabel: "15", startTimer: this.startTimer })
       ),
       React.createElement(Timer, { timeLeft: this.state.timeLeft }),
+      this.state.timeLeft > 0 && React.createElement(
+        "div",
+        null,
+        React.createElement(
+          "div",
+          { className: "btn-group", role: "group" },
+          this.state.timer === null ? React.createElement(
+            "button",
+            { className: "btn-success btn", onClick: this.resumeTimer },
+            "Resume"
+          ) : React.createElement(
+            "button",
+            { className: "btn-warning btn", onClick: this.stopTimer },
+            "Pause"
+          )
+        )
+      ),
       React.createElement("audio", { id: "end-of-time", src: "flute_c_long_01.wav", preload: "auto" })
     );
   }
